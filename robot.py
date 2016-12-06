43b7bb6384b78e110677d224aaaddd8bd10a1b05
caobang <602098328@qq.com>
2016-12-06 12:58:16 +0800

    Init my first app4

--- robot.py
+++ robot.py
@@ -3,7 +3,7 @@ from werobot import WeRoBot
 from werobot.session.saekvstorage import SaeKVDBStorage
 
 session_storage = SaeKVDBStorage()
-robot = WeRoBot(token="caobang", enable_session=True)
+robot = WeRoBot(token="caobang", enable_session=True,session_storage=session_storage)
 
 @robot.text
 def echo(message):
