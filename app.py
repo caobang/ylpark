44167021a3b5881d7111a1fbb9aa299830fe4561
caobang <602098328@qq.com>
2016-12-06 11:54:54 +0800

    Init my first app

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+import sys
+root = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(root, 'site-packages'))
+sys.path.insert(1, os.path.join(root, 'Lib/site-packages'))
+
+from flask import Flask
+from robot import robot
+from werobot.contrib.flask import make_view
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/weixin/', methods = ['GET', 'POST'])
+def weixin():
+	return make_view(robot)
+
+@app.route('/hello/')
+def hello():
+	return 'hello'
+
+if __name__ == '__main__':
+	app.run()
---
+++ robot.py
@@ -0,0 +1,13 @@
+
+
+import werobot from WeRoBot
+from werobot.session.saekvstorage import SaeKVDBStorage
+
+session_storage = SaeKVDBStorage()
+robot = WeRoBot(token="caobang", enable_session=True,
+                        session_storage=session_storage)
+
+@robot.handler
+def hello(message):
+    return 'hello test'
+
