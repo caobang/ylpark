be2cf369246b0d9217852586dc5c033a33ce0ec0
caobang <602098328@qq.com>
2016-12-06 20:08:32 +0800

    asd

--- robot.py
+++ robot.py
@@ -74,15 +74,15 @@ def ditu():
 
 @robot.text
 def echo(message):
-    if message.content == "a")
+    if (message.content == "a")
         return [
-        [
-            "瑶湖天气",
-             "",
-             "http://tu.ihuan.me/api/me_all_pic_go",
-             "http://www.caiyunapp.com/h5/?lonlat=116.056053,28.670259"
-         ]
-    ]
+            [
+                "天气",
+                "",
+                "http://tu.ihuan.me/api/me_all_pic_go",
+                "http://www.caiyunapp.com/h5/?lonlat=116.056053,28.670259"
+            ]
+        ]
     else 
         return "收到消息"
 
