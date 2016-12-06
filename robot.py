3bb815a4738a7e66c92cb233f06f6981c25da870
caobang <602098328@qq.com>
2016-12-06 13:10:29 +0800

    Init my first app4

--- robot.py
+++ robot.py
@@ -5,20 +5,17 @@ from werobot.session.saekvstorage import SaeKVDBStorage
 session_storage = SaeKVDBStorage()
 robot = WeRoBot(token="caobang", enable_session=True,session_storage=session_storage)
 
+from werobot.reply import ArticlesReply, Article
+
+
 @robot.text
 def echo(message):
-    return [
-        [
-            "title",
-            "description",
-            "img",
-            "url"
-        ],
-        [
-            "whtsky",
-            "I wrote WeRoBot",
-            "https://secure.gravatar.com/avatar/0024710771815ef9b74881ab21ba4173?s=420",
-            "http://whouz.com/"
-        ]
-    ]
+   reply = ArticlesReply(message=message)
+    article = Article(
+        title="WeRoBot",
+        description="WeRoBot是一个微信机器人框架",
+        img="https://github.com/apple-touch-icon-144.png",
+        url="https://github.com/whtsky/WeRoBot"
+    )
+    reply.add_article(article)
 
