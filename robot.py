7ffebac2be29de5ec7564b7122ca971ed792ca49
caobang <602098328@qq.com>
2016-12-06 15:02:35 +0800

    Init my first app4

--- robot.py
+++ robot.py
@@ -13,12 +13,6 @@ def tianqi():
         [
             "瑶湖天气",
             "【当前】晴",
-            "img",
-            "url"
-        ],
-        [
-            "whtsky",
-            "I wrote WeRoBot",
             "https://mmbiz.qlogo.cn/mmbiz/kic6WM8B8jd41FXzEMpTpxRe8HIMUOeTyHrsBFjkyL26ObwMEGhdOYLiaRCyvH0iahpgABLRsCk5sbezbBbWaXTibQ/0?wx_fmt=jpeg",
             "http://www.caiyunapp.com/h5/?lonlat=116.056053,28.670259"
         ]
