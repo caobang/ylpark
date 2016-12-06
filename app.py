966a363dfea3229cb70dd570583495cb14a32791
caobang <602098328@qq.com>
2016-12-06 12:01:31 +0800

    Init my first app4

--- app.py
+++ app.py
@@ -4,17 +4,14 @@ root = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(root, 'site-packages'))
 sys.path.insert(1, os.path.join(root, 'Lib/site-packages'))
 
-from flask import Flask
 from robot import robot
-from werobot.contrib.flask import make_view
+from bottle import Bottle
+from werobot.contrib.bottle import make_view
 
-app = Flask(__name__)
-app.debug = True
-
-app.add_url_rule(rule='/weixin/',
-                 endpoint='werobot',
-                 view_func=make_view(robot),
-                 methods=['GET', 'POST'])
+app = Bottle()
+app.route('/weixin/',
+         ['GET', 'POST'],
+         make_view(robot))
 
 @app.route('/hello/')
 def hello():
