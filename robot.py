805fb8102a85123a71ee1170f7adf2377fa021ca
caobang <602098328@qq.com>
2016-12-06 15:01:33 +0800

    Init my first app4

--- robot.py
+++ robot.py
@@ -9,7 +9,20 @@ robot = WeRoBot(token="caobang", enable_session=True,session_storage=session_sto
 
 @robot.filter("天气")
 def tianqi():
-    return "晴"
+    return [
+        [
+            "瑶湖天气",
+            "【当前】晴",
+            "img",
+            "url"
+        ],
+        [
+            "whtsky",
+            "I wrote WeRoBot",
+            "https://mmbiz.qlogo.cn/mmbiz/kic6WM8B8jd41FXzEMpTpxRe8HIMUOeTyHrsBFjkyL26ObwMEGhdOYLiaRCyvH0iahpgABLRsCk5sbezbBbWaXTibQ/0?wx_fmt=jpeg",
+            "http://www.caiyunapp.com/h5/?lonlat=116.056053,28.670259"
+        ]
+    ]
 
 @robot.filter("地图")
 def ditu():
