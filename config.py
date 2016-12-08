3c8e7aec4427b1ccd3ea8a8236f37ff8a20bf594
caobang <602098328@qq.com>
2016-12-08 12:10:37 +0800

    config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN="caobang"
-SESSION_STORAGE=werobot.session.saekvstorage.SaeKVDBStorage()
+# SESSION_STORAGE=werobot.session.saekvstorage.SaeKVDBStorage()
 APP_ID=None
 APP_SECRET=None
 ENCODING_AES_KEY=None
