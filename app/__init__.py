c335f625644ec55f22f8b6f26ad999ac500b395d
caobang <602098328@qq.com>
2016-12-09 14:39:31 +0800

    final

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,8 @@
 # -*- coding: UTF-8 -*-
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-
-# from config import config
-
-# db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    # app.config.from_object(config[config_name])
-    # config[config_name].init_app(app)
-
-    # db.init_app(app)
 
     from . import main,wx
     app.register_blueprint(main.route)
